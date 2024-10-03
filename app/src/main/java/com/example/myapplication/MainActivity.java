d0efe103b09cdfe5d5cc322471beda865f2a25c7
eliya <eliyacaspi4@gmail.com>
2024-10-03 16:27:24 +0300

    strings

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    String s1[], s2[];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
